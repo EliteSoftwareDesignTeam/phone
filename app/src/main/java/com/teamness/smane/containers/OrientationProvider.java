a3ca7b328bccb446296ad8d83a43cff8cd01780c
aidanGoodwin <agoodwin256@gmail.com>
2018-03-01 16:51:21 +0000

    some progress on orientation detection

--- app/src/main/java/com/teamness/smane/containers/OrientationProvider.java
+++ app/src/main/java/com/teamness/smane/containers/OrientationProvider.java
@@ -12,9 +12,23 @@ import com.teamness.smane.interfaces.IOrientationProvider;
  */
 
 public class OrientationProvider implements IOrientationProvider{
+    private SensorListener sensorListener;
+
+    public OrientationProvider(SensorManager sensorManager) {
+        Sensor magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorListener = new SensorListener();
+
+        //adds the listener
+        sensorManager.registerListener(sensorListener, magSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorListener, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
     public double getOrientation(){
-        //TODO get orientation code
-        return 0;
+        //this probably doesn't work
+        System.out.println("azi: " + sensorListener.getAzimuth());
+        System.out.println("pit: " + sensorListener.getPitch());
+        System.out.println("rol: " + sensorListener.getRoll());
+        return sensorListener.getAzimuth();
     }
 }
--- app/src/main/java/com/teamness/smane/screens/Prototype2.java
+++ app/src/main/java/com/teamness/smane/screens/Prototype2.java
@@ -1,10 +1,8 @@
 package com.teamness.smane.screens;
 
 import android.Manifest;
-import android.location.Location;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,9 +13,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.teamness.smane.R;
 import com.teamness.smane.containers.LocationProvider;
-import com.teamness.smane.containers.Route;
-import com.teamness.smane.containers.RouteFinder;
-import com.teamness.smane.containers.RouteNode;
+import com.teamness.smane.containers.OrientationProvider;
 
 public class Prototype2 extends AppCompatActivity {
     private FusedLocationProviderClient mFusedLocationClient;
@@ -25,7 +21,8 @@ public class Prototype2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        ActivityCompat.requestPermissions(this, new String [] {Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        final SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prototype2);
@@ -35,22 +32,16 @@ public class Prototype2 extends AppCompatActivity {
         final Button theButton = (Button) findViewById(R.id.thingDoer);
         final LocationProvider lp = new LocationProvider(mFusedLocationClient, this);
 
+        final OrientationProvider op = new OrientationProvider(sensorManager);
+
         theButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                RouteFinder rf = new RouteFinder();
-
-                Location currentLoc = lp.getLocation();
-
-                Route route = rf.getRouteLatLng(currentLoc.getLatitude(), currentLoc.getLongitude(), 52.450817, -1.930534);
-                System.out.println("distance: " + route.distance);
-                System.out.println();
-                for(RouteNode node: route.nodes){
-                    System.out.println(node.instruction);
-                }
-
+                op.getOrientation();
             }
 
         });
+
+        onResume();
     }
 
 }

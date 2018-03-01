bae7c4e288bc057c1492dfab39bb8ab028faa01e
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-01 15:13:16 +0000

    Added BluetoothScreen.onClick and test code

--- app/src/main/java/com/teamness/smane/screens/BluetoothScreen.java
+++ app/src/main/java/com/teamness/smane/screens/BluetoothScreen.java
@@ -2,20 +2,33 @@ package com.teamness.smane.screens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.teamness.smane.R;
 import com.teamness.smane.bluetooth.Bluetooth;
+import com.teamness.smane.bluetooth.CaneBluetooth;
+import com.teamness.smane.event.CaneEvents;
+import com.teamness.smane.event.Event;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class BluetoothScreen extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_screen);
+        try {
+            new CaneBluetooth().init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClick(View view) {
+        System.out.println("onClick");
+        CaneEvents.BT.trigger(new TestEvent());
     }
 
 }
---
+++ app/src/main/java/com/teamness/smane/screens/TestEvent.java
@@ -0,0 +1,10 @@
+package com.teamness.smane.screens;
+
+import com.teamness.smane.event.Event;
+
+/**
+ * Created by samtebbs on 28/02/2018.
+ */
+
+public class TestEvent extends Event {
+}

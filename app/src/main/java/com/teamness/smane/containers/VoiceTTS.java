7c811f80d92c86947361ef656462cf90144748b9
Mac15001900 <mac15001900@gmail.com>
2018-03-19 03:21:22 +0000

    Made voice work on the new screen, added error catching for non-existent routes

--- app/src/main/java/com/teamness/smane/containers/VoiceTTS.java
+++ app/src/main/java/com/teamness/smane/containers/VoiceTTS.java
@@ -4,12 +4,13 @@ import android.content.Context;
 import android.speech.tts.TextToSpeech;
 
 import com.teamness.smane.interfaces.IDirectionOutput;
+import com.teamness.smane.interfaces.ITextOutput;
 
 /**
  * Created by bianca on 17/02/2018.
  */
 
-public class VoiceTTS implements IDirectionOutput{
+public class VoiceTTS implements IDirectionOutput, ITextOutput {
 
     public TextToSpeech myTTS;
 
@@ -17,7 +18,7 @@ public class VoiceTTS implements IDirectionOutput{
     private String RIGHT = "Turn right";
     private String FORWARD = "Keep going forward";
 
-    public VoiceTTS(Context context, TextToSpeech.OnInitListener listener){
+    public VoiceTTS(Context context, TextToSpeech.OnInitListener listener) {
         this.myTTS = new TextToSpeech(context, listener);
     }
 
@@ -29,11 +30,16 @@ public class VoiceTTS implements IDirectionOutput{
 
     @Override
     public void giveDirection(double angle, double strength) {
-        if(angle == 0)
+        if (angle == 0)
             myTTS.speak(FORWARD, TextToSpeech.QUEUE_ADD, null);
-        else if(angle < 0)
+        else if (angle < 0)
             myTTS.speak(RIGHT, TextToSpeech.QUEUE_ADD, null);
         else
             myTTS.speak(LEFT, TextToSpeech.QUEUE_ADD, null);
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        speakWords(message);
     }
 }
--- app/src/main/java/com/teamness/smane/screens/Prototype2.java
+++ app/src/main/java/com/teamness/smane/screens/Prototype2.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -19,6 +21,7 @@ import com.teamness.smane.containers.LocationProvider;
 import com.teamness.smane.containers.Route;
 import com.teamness.smane.containers.RouteFinder;
 import com.teamness.smane.containers.TextInterpreter;
+import com.teamness.smane.containers.VoiceTTS;
 import com.teamness.smane.controller.Controller;
 import com.teamness.smane.interfaces.IDirectionOutput;
 import com.teamness.smane.interfaces.ITextOutput;
@@ -26,14 +29,19 @@ import com.teamness.smane.prototype.CommandOutput;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
-public class Prototype2 extends AppCompatActivity {
+public class Prototype2 extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
-    private static final int SPEECH_REQUEST_CODE = 0;
+    private static final int SPEECH_REQUEST_CODE = 42;
+    private static final int SYNTHESIS_REQUEST_CODE = 24601;
     private Controller controller;
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationProvider lp;
     private RouteFinder rf;
+    private List<IDirectionOutput> directionOutputs;
+    private List<ITextOutput> textOutputs;
+    private VoiceTTS voice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +50,27 @@ public class Prototype2 extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Setup TTS
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, SYNTHESIS_REQUEST_CODE);
+
         //Setup location stuff
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        ActivityCompat.requestPermissions(this, new String [] {Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         lp = new LocationProvider(mFusedLocationClient, this);
         rf = new RouteFinder();
 
 
-        List<IDirectionOutput> directionOutputs = new LinkedList<>();
+        directionOutputs = new LinkedList<>();
         directionOutputs.add(new CommandOutput());
-        List<ITextOutput> textOutputs = new LinkedList<>();
+        textOutputs = new LinkedList<>();
         textOutputs.add(new CommandOutput());
+        //if (voice == null) throw new RuntimeException("Not again...");
+        //textOutputs.add(voice);
 
-        controller = new Controller(directionOutputs,textOutputs,lp);
+
+        controller = new Controller(directionOutputs, textOutputs, lp);
 
         Button theButton = (Button) findViewById(R.id.thingDoer);
         theButton.setOnClickListener(new View.OnClickListener() {
@@ -66,7 +82,7 @@ public class Prototype2 extends AppCompatActivity {
     }
 
     /**
-     *  Call this method to start voice recognition
+     * Call this method to start voice recognition
      */
     private void displaySpeechRecognizer() {
         //System.out.println("starting");
@@ -85,37 +101,73 @@ public class Prototype2 extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
-        System.out.println("SR: "+"got result");
+        System.out.println("Results received: " + requestCode);
 
         if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
             List<String> results = data.getStringArrayListExtra(
                     RecognizerIntent.EXTRA_RESULTS);
 
-            TextInterpreter TI=new TextInterpreter();
+            TextInterpreter TI = new TextInterpreter();
 
 
-            for(int i=0;i<results.size();i++)
-            {
-                System.out.println("SR: "+results.get(i));
+            for (int i = 0; i < results.size(); i++) {
+                System.out.println("SR: " + results.get(i));
             }
 
             Command command = TI.Interpret(results);
-            switch (command.type){
+            switch (command.type) {
                 case DIRECTIONS:
                     Location currentLoc = lp.getLocation();
                     //Route route = rf.getRouteLatLng(currentLoc.getLatitude(), currentLoc.getLongitude(), 52.450817, -1.930534);
-                    System.out.println("Checking directions to "+command.content);
+                    System.out.println("Checking directions to " + command.content);
                     Route route = rf.getRoute(currentLoc.getLatitude(), currentLoc.getLongitude(), command.content);
-                    controller.startGuiding(route);
-                    System.out.println(controller.printRoute());
+                    if (route != null && route.nodes.size() > 0) {
+                        controller.startGuiding(route);
+                        System.out.println(controller.printRoute());
+                    } else {
+                        for (ITextOutput textOutput : textOutputs) {
+                            textOutput.sendMessage("Sorry, I could not find a path to " + command.content);
+                        }
+                    }
+                    break;
+                case TEST_CONNECTION:
+                    for (ITextOutput textOutput : textOutputs) {
+                        //TODO Test connection
+                        textOutput.sendMessage("I would tell you whether the bluetooth connection is working but I will need Sam to get that to work.");
+                    }
                     break;
                 default:
-                    System.err.println("Unimplemented command type "+command.type);
+                    for (ITextOutput textOutput : textOutputs) {
+                        textOutput.sendMessage("Command not recognised. Please try again.");
+                    }
+                    System.err.println("Unimplemented command type " + command.type);
             }
 
-
+        } else if (requestCode == SYNTHESIS_REQUEST_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                voice = new VoiceTTS(this, this);
+                //voice.myTTS = new TextToSpeech(this, this);
+            } else {
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    public void onInit(int i) {
+        System.err.println("onInit called with " + i);
+        if (i == TextToSpeech.SUCCESS) {
+            if (voice.myTTS.isLanguageAvailable(Locale.UK) == TextToSpeech.LANG_AVAILABLE)
+                voice.myTTS.setLanguage(Locale.UK);
+            voice.myTTS.setLanguage(Locale.UK);
+            textOutputs.add(voice);
+        } else if (i == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
+        }
     }
 
 }

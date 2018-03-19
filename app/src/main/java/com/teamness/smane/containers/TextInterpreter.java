21960b670b7e2de2761b93c4758f5f36d5bce7cd
Mac15001900 <mac15001900@gmail.com>
2018-03-19 03:19:30 +0000

    Added a command for checking if bluetooth is working

--- app/src/main/java/com/teamness/smane/containers/TextInterpreter.java
+++ app/src/main/java/com/teamness/smane/containers/TextInterpreter.java
@@ -1,6 +1,7 @@
 package com.teamness.smane.containers;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,18 +9,18 @@ import java.util.regex.Pattern;
 public class TextInterpreter {
 
     private ArrayList<String> GOcommands;
+    private ArrayList<String> TESTcommands;
     private ArrayList<String> STOPcommands;
     private ArrayList<String> SAVEcommands;
     private ArrayList<String> REPEATcommands;
 
 
-    public TextInterpreter(){
+    public TextInterpreter() {
         init();
     }
 
-    private void init()
-    {
-        GOcommands=new ArrayList<>();
+    private void init() {
+        GOcommands = new ArrayList<>();
         GOcommands.add("bring me (to )?");
         GOcommands.add("show me the way (to )?");
         GOcommands.add("navigate (to )?");
@@ -29,44 +30,50 @@ public class TextInterpreter {
         GOcommands.add("go to ");
         GOcommands.add("give me directions to ");
 
-        STOPcommands=new ArrayList<>();
+        TESTcommands = new ArrayList<>();
+        TESTcommands.add("bluetooth");
+        TESTcommands.add("connection working");
+        TESTcommands.add("is everything (ok|working)");
+
+        STOPcommands = new ArrayList<>();
         STOPcommands.add("stop");
         STOPcommands.add("cancel");
 
-        SAVEcommands=new ArrayList<>();
+        SAVEcommands = new ArrayList<>();
         SAVEcommands.add("(save|send|record|memorize|remember) this (location|place|destination) as ");
 
-        REPEATcommands=new ArrayList<>();
+        REPEATcommands = new ArrayList<>();
         REPEATcommands.add("(repeat|what)");
 
     }
 
     /**
      * This method associates a sentence with a series of known commands
+     *
      * @param texts A list of String that represent the spoken sentence
      * @return The command sting associated
      */
-    public Command Interpret(List<String> texts){
-        Command command=new Command(CommandType.NOT_IMPLEMENTED,"");
-        boolean matched=false;
+    public Command Interpret(List<String> texts) {
+        Command command = new Command(CommandType.NOT_IMPLEMENTED, "");
+        boolean matched = false;
         //Take each result obtained from speech rec, the most likely first
-        for(String text:texts){
-            text=text.toLowerCase();
-            System.out.println("TI: testing the text: "+text);
+        for (String text : texts) {
+            text = text.toLowerCase();
+            System.out.println("TI: testing the text: " + text);
             //Test for "go to" commands:
-            for(String regex:GOcommands) {
+            for (String regex : GOcommands) {
                 Pattern pattern = Pattern.compile(regex);
                 Matcher m = pattern.matcher(text);
                 if (m.find()) {
                     System.out.println("TI: " + "GO command:");
                     String dest = m.replaceFirst("");
                     System.out.println("TI: " + dest);
-                    command.reInit(CommandType.DIRECTIONS,dest);
+                    command.reInit(CommandType.DIRECTIONS, dest);
                     matched = true;
                     break;
                 }
             }
-            if(matched)break;
+            if (matched) break;
             for (String regex : SAVEcommands) {
                 Pattern pattern = Pattern.compile(regex);
                 Matcher m = pattern.matcher(text);
@@ -74,38 +81,48 @@ public class TextInterpreter {
                     System.out.println("TI: " + "SAVE command:");
                     String dest = m.replaceFirst("");
                     System.out.println("TI: " + dest);
-                    command.reInit(CommandType.SAVE_LOCATION,dest);
+                    command.reInit(CommandType.SAVE_LOCATION, dest);
                     matched = true;
                     break;
                 }
             }
-            if(matched)break;
+            if (matched) break;
             for (String regex : STOPcommands) {
                 Pattern pattern = Pattern.compile(regex);
                 Matcher m = pattern.matcher(text);
                 if (m.find()) {
                     System.out.println("TI: " + "STOP command");
-                    command.reInit(CommandType.CANCEL_DIRECTIONS,"");
+                    command.reInit(CommandType.CANCEL_DIRECTIONS, "");
                     matched = true;
                     break;
                 }
             }
-            if(matched)break;
-            for(String regex : REPEATcommands) {
+            if (matched) break;
+            for (String regex : TESTcommands) {
+                Pattern pattern = Pattern.compile(regex);
+                Matcher m = pattern.matcher(text);
+                if (m.find()) {
+                    System.out.println("TI: " + "STOP command");
+                    command.reInit(CommandType.TEST_CONNECTION, "");
+                    matched = true;
+                    break;
+                }
+            }
+            if (matched) break;
+            for (String regex : REPEATcommands) {
                 Pattern pattern = Pattern.compile(regex);
                 Matcher m = pattern.matcher(text);
                 if (m.find()) {
                     System.out.println("TI: " + "REPEAT command:");
-                    command.reInit(CommandType.REPEAT,"");
+                    command.reInit(CommandType.REPEAT, "");
                     matched = true;
                     break;
                 }
             }
-            if(matched)break;
+            if (matched) break;
         }
         return command;
     }
-
 
 
 }
